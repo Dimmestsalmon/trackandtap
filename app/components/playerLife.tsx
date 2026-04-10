2a25015cd7cb394349d5c07557aebd1ddf463480
NephiAppel <appelnephi@gmail.com>
2026-04-10 12:58:41 -0400

    added life gain and loss functionality

--- app/components/playerLife.tsx
+++ app/components/playerLife.tsx
@@ -6,13 +6,22 @@ export const LifeTotal = () => {
 
   const [life, setLife] = useState(40)
 
+  const gainLife = () => {
+    setLife(life => life+1);
+  }
+
+  const loseLife = () => {
+    setLife(life => life-1);
+  }
+
+  
  
   
   return (
     <div className='flex flex-row flex-1 grow items-center justify-center bg-yellow-300 border border-black'>
-      <button className='flex flex-1 grow items-center justify-center'>-</button>
+      <button className='flex flex-1 grow items-center justify-center' onClick={loseLife}>-</button>
       <div className='flex flex-1 grow items-center justify-center'>{life}</div>
-      <button className='flex flex-1 grow items-center justify-center'>+</button>
+      <button className='flex flex-1 grow items-center justify-center' onClick={gainLife}>+</button>
     </div>
   )
 };
