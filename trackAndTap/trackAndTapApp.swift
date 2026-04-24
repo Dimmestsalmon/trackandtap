027a6e09f9928bfd3d7b759a79b060afd6176474
NephiAppel <appelnephi@gmail.com>
2026-04-24 10:03:47 -0400

    Initial Commit

---
+++ trackAndTap/trackAndTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackAndTapApp.swift
+//  trackAndTap
+//
+//  Created by Nephi Appel on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct trackAndTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trackAndTapTests/trackAndTapTests.swift
@@ -0,0 +1,18 @@
+//
+//  trackAndTapTests.swift
+//  trackAndTapTests
+//
+//  Created by Nephi Appel on 4/24/26.
+//
+
+import Testing
+
+struct trackAndTapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
